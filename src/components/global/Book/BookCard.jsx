39193b3df976bba449f75451342e0b9ff761ee48
smit kakadiya <smitkakadiya57@gmail.com>
2023-08-01 10:12:06 +0530

    install mui and explore mui

--- src/components/global/Book/BookCard.jsx
+++ src/components/global/Book/BookCard.jsx
@@ -1,19 +1,36 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-
-const BookCard = ({title,price,author}) => {
-    const Style={
-        display:"flex",
-        gap:"15px",
-        border:"2px solid pink",
-        margin:"10px"
-    }
+const BookCard = ({ title, price, author, img }) => {
+  const Style = {
+    display: "flex",
+    gap: "15px",
+    border: "2px solid pink",
+    margin: "10px",
+  };
   return (
-    <div style={Style}>
-        <h2>{title}</h2>
-        <h4>Rs. {price}</h4>
-        <h4>{author}</h4>
-    </div>
-  )
-}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" alt="green iguana" height="140" image={img} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {author}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Typography variant="h4">Rs. {price}</Typography>
+        <Button size="small" variant="contained" sx={{ marginLeft: "10px" }}>
+          Add to Cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default BookCard
+export default BookCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
-
+import { FormControl, FormHelperText, MenuItem, Select } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
 const Home = () => {
-  return (
-    <div>Home Page</div>
-  )
-}
+  const [age, setAge] = useState("");
 
-export default Home
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        <FormHelperText>Select your age</FormHelperText>
+      </FormControl>
+    </>
+  );
+};
+
+export default Home;
